2e237809ca2081407763e91a59cc0b3a7c79cad9
Kamil Kornatowski <72910675+Kamil-Kornatowski@users.noreply.github.com>
2023-01-03 13:47:31 +0100

    Working on Main Menu layout with UI Toolkit

--- Assets/Scripts/MenuScripts/StartGame.cs
+++ Assets/Scripts/MenuScripts/StartGame.cs
@@ -1,16 +1,38 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class StartGame : MonoBehaviour
 {
- 
+    VisualElement root;
+    Button start;
 
-    public void OnButtonClick()
+    private void Start()
+    {
+        root = GetComponent<UIDocument>().rootVisualElement;
+        Button buttonStart = root.Q<Button>("Start");
+        Button buttonSettings = root.Q<Button>("Settings");
+        Button buttonExit = root.Q<Button>("Exit");
+
+
+        buttonStart.clicked += () => StartGameButton();
+    }
+
+    public void StartGameButton()
     {
         SceneManager.LoadScene("GameScene");
-        Debug.Log("Button pressed");
+        
+    } 
+    public void SettingsGameButton()
+    {
+       //Settings menu enable
+    } 
+    public void ExitGameButton()
+    {
+       Application.Quit();
+        
     }
+
+
 }

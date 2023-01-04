863fd7a3b49b496219862f40245f8b8e01650a33
Kamil Kornatowski <72910675+Kamil-Kornatowski@users.noreply.github.com>
2023-01-04 16:03:41 +0100

    Finishing work on Simple Loop, only small tweaks needed

--- Assets/Scripts/TrackElements/BoundDestroy.cs
+++ Assets/Scripts/TrackElements/BoundDestroy.cs
@@ -8,7 +8,7 @@ public class BoundDestroy : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        //SceneManager.LoadScene("GameScene");
-        Debug.Log("BAND HIT");
+        SceneManager.LoadScene("GameScene");
+        //Debug.Log("BAND HIT");
     }
 }
